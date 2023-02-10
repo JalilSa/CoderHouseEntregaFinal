aab482f2b2e03ede3f184cf022c38b22efd54d3a
JalilSa <jalilsalomon2002@gmail.com>
2023-02-10 15:28:53 -0300

    Proyecto actualizado 2

--- pages/javascript/main.js
+++ pages/javascript/main.js
@@ -3,6 +3,6 @@ const track = document.getElementById("carousel");
 
 window.onmousedown = e =>{
     track.dataset.mouseDownat = e.clientX;
-    console.log("La concha de tu madre");
+    console.log("Funciona");
 }
 
