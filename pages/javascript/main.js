447d99c4892ada6e9902ad65f0f5c8ed9345bd51
JalilSa <jalilsalomon2002@gmail.com>
2023-02-10 22:03:11 -0300

    Proyecto actualizado 5

--- pages/javascript/main.js
+++ pages/javascript/main.js
@@ -1,8 +1,24 @@
-const track = document.getElementById("carousel");
-
-
-if( track!=null){ window.onmousedown = e =>{
+window.onload = function() {
+  const track = document.getElementById("carousel");
+  window.onmousedown = e => {
     track.dataset.mouseDownat = e.clientX;
-    console.log("corre");
-}
- }
+  }
+  window.onmouseup = () =>{
+    track.dataset.mouseDownat="0";
+
+    track.dataset.prevpercentage= track.dataset.percentage;
+  }
+   window.onmousemove = e =>{
+    if(track.dataset.mouseDownat=== "0") return;
+
+      const mousedelta = parseFloat(track.dataset.mouseDownat)- e.clientX;
+          maxdelta = window.innerWidth/2;
+
+          const percentage = (mousedelta/ maxdelta)*100;
+
+          nextpercentage = parseFloat(track.dataset.prevpercentage)+percentage;
+
+          track.dataset.percentage=nextpercentage;
+          track.style.transform = "translate(" + percentage + "%,-50%)";
+   }
+  };
