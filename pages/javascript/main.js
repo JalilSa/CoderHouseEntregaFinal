9261fc975b4a1c3e38ef542b035a275e479892dc
JalilSa <jalilsalomon2002@gmail.com>
2023-02-14 15:24:34 -0300

    Entrega Final test

--- pages/javascript/main.js
+++ pages/javascript/main.js
@@ -1,6 +1,11 @@
+
+
 window.onload = function() {
   const track = document.getElementById("carousel");
   track.dataset.prevpercentage = 0;
+  var nextpercentage = 0;
+  Math.min(0, nextpercentage);
+ Math.max(-100, nextpercentage);
   window.onmousedown = e => {
     track.dataset.mouseDownat = e.clientX;
   }
@@ -10,16 +15,14 @@ window.onload = function() {
     track.dataset.prevpercentage = track.dataset.percentage;
   }
    window.onmousemove = e =>{
-    if(track.dataset.mouseDownat=== "0") return;
+    if(track.dataset.mouseDownat === "0") return;
 
       const mousedelta = parseFloat(track.dataset.mouseDownat)- e.clientX;
           maxdelta = window.innerWidth/2;
 
-    const percentage = (mousedelta/ maxdelta)*100;
+    const percentage = (mousedelta/ maxdelta)*-100;
            nextpercentage = parseFloat(track.dataset.prevpercentage)+percentage;
            track.dataset.percentage=nextpercentage
-           Math.min(-100,nextpercentage);
-           Math.max(0, nextpercentage);
 
 
 
@@ -27,8 +30,7 @@ track.animate({
   transform:
   "translate(" + nextpercentage + "%,-50%)"},{duration:1200, fill:"forwards"}
 
-)
-
+)    
         } 
     }
 
